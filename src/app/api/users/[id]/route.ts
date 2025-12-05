d0e46d7b043e28c32cfc1a3cfd46e328d31b54c6
AH-2002 <xabdo608@example.com>
2025-12-05 17:32:04 +0200

    working on params

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -2,21 +2,16 @@ import { getCollection } from "@/lib/db";
 import { NextResponse } from "next/server";
 import { ObjectId } from "mongodb";
 
-interface Context {
-  params: {
-    id: string;
-  };
-}
+export async function GET(req: Request, context: any) {
+  const { params } = context as { params: { id: string } };
 
-export async function GET(req: Request, context: Context) {
-  const id = context.params.id;
+  const { id } = params;
 
   const usersCollection = await getCollection("users");
   const user = await usersCollection?.findOne({ _id: new ObjectId(id) });
 
-  if (!user) {
+  if (!user)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
-  }
 
   return NextResponse.json(user);
 }
