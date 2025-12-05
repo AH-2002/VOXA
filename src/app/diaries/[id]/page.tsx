02852bccaf6c12ff2000a64b09cf24f7cfdc4121
AH-2002 <xabdo608@example.com>
2025-12-05 17:39:18 +0200

    working on diary

--- src/app/diaries/[id]/page.tsx
+++ src/app/diaries/[id]/page.tsx
@@ -4,20 +4,24 @@ import { dbDiariesType } from "../types";
 import { serializeDiary } from "@/lib/serialize";
 import DiaryCard from "@/app/shared/ui/cards/diary-card";
 
-type Params = {
+export default async function DiaryDetails({
+  params,
+}: {
   params: { id: string };
-};
-export default async function DiaryDetails(props: Params) {
-  const { params } = await props;
-  const { id } = await params;
+}) {
+  const id = params.id;
+
   const diariesCollection = await getCollection("diaries");
   const rawDiary = (await diariesCollection?.findOne({
     _id: new ObjectId(id),
   })) as dbDiariesType;
+
   const diary = serializeDiary(rawDiary);
+
   if (!diary) {
     return <div>Diary not found</div>;
   }
+
   return (
     <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
       <DiaryCard detailsPage={true} diary={diary} />
