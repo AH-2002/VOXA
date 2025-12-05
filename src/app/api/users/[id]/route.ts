9411f3083154893f1a0274751e156255130fc138
AH-2002 <xabdo608@example.com>
2025-12-05 17:25:11 +0200

    working on user route params

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -2,11 +2,21 @@ import { getCollection } from "@/lib/db";
 import { NextResponse } from "next/server";
 import { ObjectId } from "mongodb";
 
-export async function GET(req: Request, context: { params: { id: string } }) {
-  const { id } = context.params;
+interface Context {
+  params: {
+    id: string;
+  };
+}
+
+export async function GET(req: Request, context: Context) {
+  const id = context.params.id;
+
   const usersCollection = await getCollection("users");
   const user = await usersCollection?.findOne({ _id: new ObjectId(id) });
-  if (!user)
+
+  if (!user) {
     return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
+
   return NextResponse.json(user);
 }
